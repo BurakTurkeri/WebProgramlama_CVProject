393c499df559c7c8fe0e817c3f9ecd6546c2345f
BurakTurkeri <burakturkeriii@gmail.com>
2018-04-04 17:31:22 +0300

    Odev

---
+++ CV_Project/App_Start/RouteConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace CV_Project
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute("Home", "", new { Controller = "information", Action = "AboutMe" });
+            routes.MapRoute("RSkills", "RSkills", new { Controller = "information", Action = "Skills" });
+            routes.MapRoute("RExperience", "RExperience", new { Controller = "information", Action = "Experience" });
+            routes.MapRoute("RContactMe", "RContactMe", new { Controller = "information", Action = "ContactMe" });
+        }
+    }
+}
---
+++ CV_Project/Controllers/informationController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CV_Project.Controllers
+{
+    public class informationController : Controller
+    {
+        // GET: information
+        public ActionResult AboutMe()
+        {
+            return View();
+        }
+
+        public ActionResult Skills()
+        {
+            return View();
+        }
+
+        public ActionResult Experience()
+        {
+            return View();
+        }
+
+        public ActionResult ContactMe()
+        {
+            return View();
+        }
+    }
+}
